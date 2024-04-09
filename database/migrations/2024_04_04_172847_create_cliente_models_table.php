d2542c434d1ba9f4c1964d6710ef0e40ff3e5815
gp0987gp <gp0987gp@gmail.com>
2024-04-09 14:20:09 -0300

    att

--- database/migrations/2024_04_04_172847_create_cliente_models_table.php
+++ database/migrations/2024_04_04_172847_create_cliente_models_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->string('foto')->nullable(false);
 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('produtos');
+        Schema::dropIfExists('clientes');
     }
 };
